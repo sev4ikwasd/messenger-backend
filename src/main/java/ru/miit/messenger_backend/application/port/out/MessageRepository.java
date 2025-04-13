7e1f0132d711d7bde84038742e24361ce36e51f6
Vsevolod Bezaev <vvbezaev@gmail.com>
2025-04-14 00:12:31 +0300

    fix: Db requests

--- src/main/java/ru/miit/messenger_backend/application/port/out/MessageRepository.java
+++ src/main/java/ru/miit/messenger_backend/application/port/out/MessageRepository.java
@@ -30,15 +30,15 @@ public interface MessageRepository extends CrudRepository<Message, Integer> {
     @Query("SELECT count(*) FROM messenger.message WHERE ou_group = :ou AND (((id_state = 1) AND (id_receiver = :id)) OR (id_receiver <> :id))")
     int getOldUserGroupMessagesSize(int id, String ou);
 
-    @Query("SELECT * FROM messenger.message WHERE ou_group IS NULL AND ((id_sender = :id AND id_receiver = :otherUserId) OR (id_sender = :otherUserId AND id_receiver = :id)) AND (((id_state = 0) AND (id_receiver = :id)) OR (id_receiver <> :id)) ORDER BY time_sent ASC LIMIT :size OFFSET :offset")
+    @Query("SELECT * FROM messenger.message WHERE ou_group IS NULL AND ((id_sender = :id AND id_receiver = :otherUserId) OR (id_sender = :otherUserId AND id_receiver = :id)) AND id_state = 0 ORDER BY time_sent ASC LIMIT :size OFFSET :offset")
     List<Message> getNewUserChatMessages(int id, int otherUserId, int size, int offset);
 
-    @Query("SELECT count(*) FROM messenger.message WHERE ou_group IS NULL AND ((id_sender = :id AND id_receiver = :otherUserId) OR (id_sender = :otherUserId AND id_receiver = :id)) AND (((id_state = 0) AND (id_receiver = :id)) OR (id_receiver <> :id))")
+    @Query("SELECT count(*) FROM messenger.message WHERE ou_group IS NULL AND ((id_sender = :id AND id_receiver = :otherUserId) OR (id_sender = :otherUserId AND id_receiver = :id)) AND id_state = 0")
     int getNewUserChatMessagesSize(int id, int otherUserId);
 
-    @Query("SELECT * FROM messenger.message WHERE ou_group = :ou AND (((id_state = 0) AND (id_receiver = :id)) OR (id_receiver <> :id)) ORDER BY time_sent ASC LIMIT :size OFFSET :offset")
+    @Query("SELECT * FROM messenger.message WHERE ou_group = :ou AND id_state = 0 AND (id_receiver = :id OR id_receiver <> :id) ORDER BY time_sent ASC LIMIT :size OFFSET :offset")
     List<Message> getNewUserGroupMessages(int id, String ou, int size, int offset);
 
-    @Query("SELECT count(*) FROM messenger.message WHERE ou_group = :ou AND (((id_state = 0) AND (id_receiver = :id)) OR (id_receiver <> :id))")
+    @Query("SELECT count(*) FROM messenger.message WHERE ou_group = :ou AND id_state = 0 AND (id_receiver = :id OR id_receiver <> :id)")
     int getNewUserGroupMessagesSize(int id, String ou);
 }
