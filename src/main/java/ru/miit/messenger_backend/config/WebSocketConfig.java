9400635f4c83965ce30516d7c03cbd6c4ef5b360
Vsevolod Bezaev <vvbezaev@gmail.com>
2025-05-23 13:16:18 +0300

    fix: Websocket message size

--- src/main/java/ru/miit/messenger_backend/config/WebSocketConfig.java
+++ src/main/java/ru/miit/messenger_backend/config/WebSocketConfig.java
@@ -1,11 +1,14 @@
 package ru.miit.messenger_backend.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import ru.miit.messenger_backend.config.auth.JwtChannelInterceptor;
 
 @Configuration
@@ -31,5 +34,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(jwtChannelInterceptor);
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.setMessageSizeLimit(1024 * 1024 * 20);
+        registry.setSendTimeLimit(40 * 10000);
+        registry.setSendBufferSizeLimit(3 * 512 * 1024);
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(1024 * 1024 * 20);
+        container.setMaxBinaryMessageBufferSize(1024 * 1024 * 20);
+        return container;
     }
 }
